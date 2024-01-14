b118e57c21b589e2d7e90ebc237ab50972b99e0c
Erika Bautista <ekabells19@gmail.com>
2024-01-15 01:51:33 +0800

    Initial commit

---
+++ src/main/java/com/mycompany/songartistalbum/SongArtistAlbum.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.songartistalbum;
+
+/**
+ *
+ * @author Erika
+ */
+public class SongArtistAlbum {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
